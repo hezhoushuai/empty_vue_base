7596450590e95dbb033a2b7a4c79f82bfba36239
hezhoushuai <993578319@qq.com>
2019-10-07 11:35:37 +0800

    添加echarts

---
+++ src/components/PieChart/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div ref="pieChart" :style="{width: '400px', height: '400px'}"></div>
+</template>
+
+<script>
+const echarts = require('echarts')
+export default {
+  components: {},
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  watch: {},
+  mounted() {
+    this.initDraw()
+  },
+  created() {
+
+  },
+  methods: {
+    initDraw() {
+      const myCharts = echarts.init(this.$refs.pieChart)
+      const option = {
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b}: {c} ({d}%)'
+        },
+        legend: {
+          orient: 'vertical',
+          x: 'left',
+          data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: ['50%', '70%'],
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                show: false,
+                position: 'center'
+              },
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: '30',
+                  fontWeight: 'bold'
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            data: [
+              { value: 335, name: '直接访问' },
+              { value: 310, name: '邮件营销' },
+              { value: 234, name: '联盟广告' },
+              { value: 135, name: '视频广告' },
+              { value: 1548, name: '搜索引擎' }
+            ]
+          }
+        ]
+      }
+
+      myCharts.setOption(option)
+    }
+  }
+}
+</script>
+<style lang='scss' scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,19 @@
   <div class="home">
     <el-button type="danger">test element</el-button>
     <el-slider v-model="value"></el-slider>
-    <div id="main" ref="main" :style="{width: '300px', height: '300px'}"></div>
+    <div class="box">
+      <PieChart />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-const echarts = require('echarts')
-
+import PieChart from '@/components/PieChart'
 export default {
   name: 'Home',
   components: {
+    PieChart
   },
   data() {
     return {
@@ -20,30 +22,9 @@ export default {
     }
   },
   mounted() {
-    this.drawInit()
   },
   methods: {
-    drawInit() {
-      const main = this.$refs.main
-      const myChart = echarts.init(main)
 
-      myChart.setOption({
-        title: {
-          text: 'ECharts 入门示例'
-        },
-        tooltip: {},
-        xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-        },
-        yAxis: {},
-        series: [{
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }]
-      })
-      console.log(myChart)
-    }
   }
 }
 </script>
